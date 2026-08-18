73a98b15b10d67a6d726b1afcde9b3a8d8ceab60
Ezio丶zH <2750821628@qq.com>
2026-08-18 19:23:24 +0800

    梦开始的地方：HelloWorld程序

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,8 +6,8 @@ public class HelloWorld {
 // CTRL + / 单行注释
 /* CTRL + SHIFT + / 多行注释 */
 
-    public static void main(String[] args) {    //psvm
-        System.out.println("Hello World");      //sout
+    public static void main(String[] args) {    //快捷输入psvm
+        System.out.println("Hello World");      //快捷输入sout
     }
 }
 
