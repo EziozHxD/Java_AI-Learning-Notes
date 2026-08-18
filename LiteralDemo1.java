425025bafc963a2eb50f2538cc76cb830151070c
Ezio丶zH <2750821628@qq.com>
2026-08-18 18:46:32 +0800

    字面量基础输出

---
+++ LiteralDemo1.java
@@ -0,0 +1,19 @@
+package com.Ezio.literal;
+
+public class LiteralDemo1 {
+    public static void main(String[] args) {
+        // 输出野兽先辈的信息：田所浩二 24岁 男 学生
+
+        // 1.输出野兽先辈的名字
+        System.out.println("田所浩二");
+
+        // 2.输出野兽先辈的年龄
+        System.out.println(24);
+
+        // 3.输出野兽先辈的性别
+        System.out.println('男');
+
+        // 4.输出野兽先辈的职业
+        System.out.println("是学生");
+    }
+}
