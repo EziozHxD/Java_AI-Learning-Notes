daed512d0eb2ed9db1ec6aa9e588d2d7ed937945
EziozHxD <2750821628@qq.com>
2026-09-05 12:44:42 +0800

    if基本格式

---
+++ src/com/Ezio/ifdemo/IfDemo1.java
@@ -0,0 +1,13 @@
+package com.Ezio.ifdemo;
+
+public class IfDemo1 {
+    public static void main(String[] args) {
+
+        // 定义变量表示san值，当san值大于200的时候就会变成赛博精神病。
+        int san = 277;
+        if (san > 200) {
+            System.out.println("你的san值是：" + san);
+            System.out.println("你变成了赛博精神病");
+        }
+    }
+}
